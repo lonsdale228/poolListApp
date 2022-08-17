7f93fa2eca60b20d485fc672c68963d167448b4d
lonsdale228 <0193493@mail.ru>
2022-08-18 01:22:56 +0300

    first push

--- app/src/main/java/com/example/poollistapp/adapters/ServicesAdapter.kt
+++ app/src/main/java/com/example/poollistapp/adapters/ServicesAdapter.kt
@@ -15,7 +15,7 @@ class ServicesAdapter(private val items:List<Services>):RecyclerView.Adapter<Ser
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ServicesViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.services_single_item,parent,false)
         return ServicesViewHolder(view)
-        //sss
+        //sss1
     }
 
     override fun onBindViewHolder(holder: ServicesViewHolder, position: Int) {
